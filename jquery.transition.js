afdfae783f3bfabed34accf582ff0c14beefba3d
hirata <hirata@team-lab.com>
2016-06-19 00:17:46 +0900

    transitionIn追加

--- jquery.transition.js
+++ jquery.transition.js
@@ -17,6 +17,22 @@
 	}
 })();
 
+$.extend({
+	clipShape : function(ctx, type, width, height, rate){
+		if(type === 'linear-left-bottom'){
+			ctx.moveTo(0, height);
+			ctx.lineTo(0, height - (width + height) * rate);
+			ctx.lineTo((width + height) * rate, height);
+		}
+		// default linear-left-top
+		else{
+			ctx.moveTo(0, 0);
+			ctx.lineTo(0, (width + height) * rate);
+			ctx.lineTo((width + height) * rate, 0);
+		}
+	}
+})
+
 // add jquery function
 $.fn.extend({
 	transition : function(opt){
@@ -27,6 +43,7 @@ $.fn.extend({
 		var opt = $.extend({
 			fps : 60,
 			duration : 500,
+			clip : 'linear-left-top',
 			easing : typeof $.easing.swing === 'function' && $.easing.swing.length == 5 ? $.easing.swing : function(x, t, b, c, d){ return.5-Math.cos(x*Math.PI)/2; }
 		}, opt);
 
@@ -81,9 +98,7 @@ $.fn.extend({
 				ctx.drawImage(backgroundImage, 0, 0);
 				ctx.save();
 				ctx.beginPath();
-				ctx.moveTo(0, 0);
-				ctx.lineTo(0, (width + height) * easingRate);
-				ctx.lineTo((width + height) * easingRate, 0);
+				$.clipShape(ctx, opt.clip, width, height, easingRate);
 				ctx.closePath();
 				ctx.clip();
 				ctx.drawImage(hoverImage, 0, 0);
@@ -102,6 +117,86 @@ $.fn.extend({
 				}
 			};
 		});
+
+		return this;
+	},
+
+	transitionIn : function(opt, callback){
+		var $self = $(this);
+		var $img = $(this).children();
+
+		// option
+		var opt = $.extend({
+			fps : 60,
+			duration : 500,
+			easing : typeof $.easing.swing === 'function' && $.easing.swing.length == 5 ? $.easing.swing : function(x, t, b, c, d){ return.5-Math.cos(x*Math.PI)/2; }
+		}, opt);
+
+		// var
+		var animationTimeout = -1;
+		var hodlerPosition = 0;
+
+		// this element has 1 <img> only
+		if(!($img.length === 1 && $img.is('img'))){
+			return this;
+		}
+
+		// get <img> size
+		if($img[0].complete){
+			startTransitionIn();
+		}else{
+			$img.bind('load', startTransitionIn);
+		}
+
+		function startTransitionIn(){
+			var hoverImage = $img[0];
+			var width = $img.width();
+			var height = $img.height();
+
+			// create <canvas>
+			var canvas = document.createElement('canvas');
+			canvas.width = width;
+			canvas.height = height;
+
+			// replace <img> -> <canvas>
+			var ctx = canvas.getContext('2d');
+			$self.append(canvas);
+			$img.remove();
+
+			// load hover image
+			animation(hodlerPosition, 1);
+
+			// animation method
+			function animation(position, sign){
+				var rangePosition = Math.max(0, Math.min(opt.duration, position));
+				var easingRate = opt.easing(rangePosition / opt.duration, rangePosition, 0, 1, opt.duration);
+
+				ctx.clearRect(0, 0, width, height);
+				ctx.save();
+				ctx.beginPath();
+				$.clipShape(ctx, opt.clip, width, height, easingRate);
+				ctx.closePath();
+				ctx.clip();
+				ctx.drawImage(hoverImage, 0, 0);
+				ctx.restore();
+
+				if(position < 0){
+					position = 0;
+				}else if(position > opt.duration){
+					position = opt.duration;
+
+					if(typeof callback === 'function'){
+						callback.apply($self, []);
+					}
+				}else{
+					var delay = 1000 / opt.fps;
+					hodlerPosition = position;
+					animationTimeout = setTimeout(function(){
+						animation(position + delay * sign, sign);
+					}, delay);
+				}
+			}
+		}
 
 		return this;
 	}
