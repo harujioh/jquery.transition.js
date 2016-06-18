d2cd515834aac9ac78bb91d3d90b8f496a6869a4
hirata <hirata@team-lab.com>
2016-06-19 00:19:07 +0900

    複数あった時のバグ対策

--- jquery.transition.js
+++ jquery.transition.js
@@ -36,167 +36,171 @@ $.extend({
 // add jquery function
 $.fn.extend({
 	transition : function(opt){
-		var $self = $(this);
-		var $img = $(this).children();
+		$(this).each(function(){
+			var $self = $(this);
+			var $img = $(this).children();
 
-		// option
-		var opt = $.extend({
-			fps : 60,
-			duration : 500,
-			clip : 'linear-left-top',
-			easing : typeof $.easing.swing === 'function' && $.easing.swing.length == 5 ? $.easing.swing : function(x, t, b, c, d){ return.5-Math.cos(x*Math.PI)/2; }
-		}, opt);
+			// option
+			var opt = $.extend({
+				fps : 60,
+				duration : 500,
+				clip : 'linear-left-top',
+				easing : typeof $.easing.swing === 'function' && $.easing.swing.length == 5 ? $.easing.swing : function(x, t, b, c, d){ return.5-Math.cos(x*Math.PI)/2; }
+			}, opt);
 
-		// var
-		var animationTimeout = -1;
-		var hodlerPosition = 0;
+			// var
+			var animationTimeout = -1;
+			var hodlerPosition = 0;
 
-		// this element has 1 <img> only
-		if(!($img.length === 1 && $img.is('img'))){
-			return this;
-		}
-
-		// get <img> size
-		$img.bind('load', function(){
-			var backgroundImage = $(this)[0];
-			var width = $(this).width();
-			var height = $(this).height();
-
-			// create <canvas>
-			var canvas = document.createElement('canvas');
-			canvas.width = width;
-			canvas.height = height;
-
-			// replace <img> -> <canvas>
-			var ctx = canvas.getContext('2d');
-			ctx.drawImage(backgroundImage, 0, 0);
-			$self.append(canvas);
-			$(this).remove();
-
-			// load hover image
-			var hoverImageSrc = $self.data('hoverImage') || '';
-			if(hoverImageSrc.length > 0){
-				var hoverImage = new Image();
-				hoverImage.onload = function(){
-					$self.hover(function(){
-						clearTimeout(animationTimeout);
-						animation(hodlerPosition, 1);
-					}, function(){
-						clearTimeout(animationTimeout);
-						animation(hodlerPosition, -1);
-					});
-				}
-				hoverImage.src = hoverImageSrc;
+			// this element has 1 <img> only
+			if(!($img.length === 1 && $img.is('img'))){
+				return this;
 			}
 
-			// animation method
-			function animation(position, sign){
-				var rangePosition = Math.max(0, Math.min(opt.duration, position));
-				var easingRate = opt.easing(rangePosition / opt.duration, rangePosition, 0, 1, opt.duration);
+			// get <img> size
+			$img.bind('load', function(){
+				var backgroundImage = $(this)[0];
+				var width = $(this).width();
+				var height = $(this).height();
 
-				ctx.clearRect(0, 0, width, height);
+				// create <canvas>
+				var canvas = document.createElement('canvas');
+				canvas.width = width;
+				canvas.height = height;
+
+				// replace <img> -> <canvas>
+				var ctx = canvas.getContext('2d');
 				ctx.drawImage(backgroundImage, 0, 0);
-				ctx.save();
-				ctx.beginPath();
-				$.clipShape(ctx, opt.clip, width, height, easingRate);
-				ctx.closePath();
-				ctx.clip();
-				ctx.drawImage(hoverImage, 0, 0);
-				ctx.restore();
+				$self.append(canvas);
+				$(this).remove();
 
-				if(position < 0){
-					position = 0;
-				}else if(position > opt.duration){
-					position = opt.duration;
-				}else{
-					var delay = 1000 / opt.fps;
-					hodlerPosition = position;
-					animationTimeout = setTimeout(function(){
-						animation(position + delay * sign, sign);
-					}, delay);
+				// load hover image
+				var hoverImageSrc = $self.data('hoverImage') || '';
+				if(hoverImageSrc.length > 0){
+					var hoverImage = new Image();
+					hoverImage.onload = function(){
+						$self.hover(function(){
+							clearTimeout(animationTimeout);
+							animation(hodlerPosition, 1);
+						}, function(){
+							clearTimeout(animationTimeout);
+							animation(hodlerPosition, -1);
+						});
+					}
+					hoverImage.src = hoverImageSrc;
 				}
-			};
+
+				// animation method
+				function animation(position, sign){
+					var rangePosition = Math.max(0, Math.min(opt.duration, position));
+					var easingRate = opt.easing(rangePosition / opt.duration, rangePosition, 0, 1, opt.duration);
+
+					ctx.clearRect(0, 0, width, height);
+					ctx.drawImage(backgroundImage, 0, 0);
+					ctx.save();
+					ctx.beginPath();
+					$.clipShape(ctx, opt.clip, width, height, easingRate);
+					ctx.closePath();
+					ctx.clip();
+					ctx.drawImage(hoverImage, 0, 0);
+					ctx.restore();
+
+					if(position < 0){
+						position = 0;
+					}else if(position > opt.duration){
+						position = opt.duration;
+					}else{
+						var delay = 1000 / opt.fps;
+						hodlerPosition = position;
+						animationTimeout = setTimeout(function(){
+							animation(position + delay * sign, sign);
+						}, delay);
+					}
+				};
+			});
 		});
 
 		return this;
 	},
 
 	transitionIn : function(opt, callback){
-		var $self = $(this);
-		var $img = $(this).children();
+		$(this).each(function(){
+			var $self = $(this);
+			var $img = $(this).children();
 
-		// option
-		var opt = $.extend({
-			fps : 60,
-			duration : 500,
-			easing : typeof $.easing.swing === 'function' && $.easing.swing.length == 5 ? $.easing.swing : function(x, t, b, c, d){ return.5-Math.cos(x*Math.PI)/2; }
-		}, opt);
+			// option
+			var opt = $.extend({
+				fps : 60,
+				duration : 500,
+				easing : typeof $.easing.swing === 'function' && $.easing.swing.length == 5 ? $.easing.swing : function(x, t, b, c, d){ return.5-Math.cos(x*Math.PI)/2; }
+			}, opt);
 
-		// var
-		var animationTimeout = -1;
-		var hodlerPosition = 0;
+			// var
+			var animationTimeout = -1;
+			var hodlerPosition = 0;
 
-		// this element has 1 <img> only
-		if(!($img.length === 1 && $img.is('img'))){
-			return this;
-		}
+			// this element has 1 <img> only
+			if(!($img.length === 1 && $img.is('img'))){
+				return this;
+			}
 
-		// get <img> size
-		if($img[0].complete){
-			startTransitionIn();
-		}else{
-			$img.bind('load', startTransitionIn);
-		}
+			// get <img> size
+			if($img[0].complete){
+				startTransitionIn();
+			}else{
+				$img.bind('load', startTransitionIn);
+			}
 
-		function startTransitionIn(){
-			var hoverImage = $img[0];
-			var width = $img.width();
-			var height = $img.height();
+			function startTransitionIn(){
+				var hoverImage = $img[0];
+				var width = $img.width();
+				var height = $img.height();
 
-			// create <canvas>
-			var canvas = document.createElement('canvas');
-			canvas.width = width;
-			canvas.height = height;
+				// create <canvas>
+				var canvas = document.createElement('canvas');
+				canvas.width = width;
+				canvas.height = height;
 
-			// replace <img> -> <canvas>
-			var ctx = canvas.getContext('2d');
-			$self.append(canvas);
-			$img.remove();
+				// replace <img> -> <canvas>
+				var ctx = canvas.getContext('2d');
+				$self.append(canvas);
+				$img.remove();
 
-			// load hover image
-			animation(hodlerPosition, 1);
+				// load hover image
+				animation(hodlerPosition, 1);
 
-			// animation method
-			function animation(position, sign){
-				var rangePosition = Math.max(0, Math.min(opt.duration, position));
-				var easingRate = opt.easing(rangePosition / opt.duration, rangePosition, 0, 1, opt.duration);
+				// animation method
+				function animation(position, sign){
+					var rangePosition = Math.max(0, Math.min(opt.duration, position));
+					var easingRate = opt.easing(rangePosition / opt.duration, rangePosition, 0, 1, opt.duration);
 
-				ctx.clearRect(0, 0, width, height);
-				ctx.save();
-				ctx.beginPath();
-				$.clipShape(ctx, opt.clip, width, height, easingRate);
-				ctx.closePath();
-				ctx.clip();
-				ctx.drawImage(hoverImage, 0, 0);
-				ctx.restore();
+					ctx.clearRect(0, 0, width, height);
+					ctx.save();
+					ctx.beginPath();
+					$.clipShape(ctx, opt.clip, width, height, easingRate);
+					ctx.closePath();
+					ctx.clip();
+					ctx.drawImage(hoverImage, 0, 0);
+					ctx.restore();
 
-				if(position < 0){
-					position = 0;
-				}else if(position > opt.duration){
-					position = opt.duration;
+					if(position < 0){
+						position = 0;
+					}else if(position > opt.duration){
+						position = opt.duration;
 
-					if(typeof callback === 'function'){
-						callback.apply($self, []);
+						if(typeof callback === 'function'){
+							callback.apply($self, []);
+						}
+					}else{
+						var delay = 1000 / opt.fps;
+						hodlerPosition = position;
+						animationTimeout = setTimeout(function(){
+							animation(position + delay * sign, sign);
+						}, delay);
 					}
-				}else{
-					var delay = 1000 / opt.fps;
-					hodlerPosition = position;
-					animationTimeout = setTimeout(function(){
-						animation(position + delay * sign, sign);
-					}, delay);
 				}
 			}
-		}
+		});
 
 		return this;
 	}
